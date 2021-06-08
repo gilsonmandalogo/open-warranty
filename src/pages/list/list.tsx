cbf7afad352ab0b638f9d64c688baab4720019ba
Gilson Júnior <gilsonmandalogo@hotmail.com>
2021-06-08 22:39:14 +0100

    Add confirmation modal to delete mutation

--- src/pages/list/list.tsx
+++ src/pages/list/list.tsx
@@ -1,9 +1,10 @@
 import { Loading } from 'components/Loading';
 import { useLoadingOverlayContext } from 'contexts/LoadingOverlay';
-import { useDeleteInvoiceMutation, useInvoiceAllQuery } from 'generated/graphql';
+import { InvoiceAllResult, useDeleteInvoiceMutation, useInvoiceAllQuery } from 'generated/graphql';
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
+import Modal from 'react-bootstrap/Modal';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import ProgressBar from 'react-bootstrap/ProgressBar';
 import Spinner from 'react-bootstrap/Spinner';
@@ -12,10 +13,13 @@ import Tooltip from 'react-bootstrap/Tooltip';
 import { BsArrowRepeat, BsImage, BsPencilSquare, BsTrash } from 'react-icons/bs';
 import styles from './list.module.scss';
 
+type ItemToDelete = Pick<InvoiceAllResult, 'id' | 'item'>;
+
 const List = () => {
   const invoiceQuery = useInvoiceAllQuery();
   const [deleteInvoiceMutation, deleteInvoiceResult] = useDeleteInvoiceMutation();
   const [dataPage, setDataPage] = React.useState(1);
+  const [itemToDelete, setItemToDelete] = React.useState<ItemToDelete | null>(null);
   const [loadingMore, setLoadingMore] = React.useState(false);
   const setIsLoading = useLoadingOverlayContext();
 
@@ -34,15 +38,26 @@ const List = () => {
     setDataPage(dataPage + 1);
   }, [invoiceQuery, dataPage]);
 
-  const handleDelete = React.useCallback((id: string) => async () => {
+  const handleDelete = React.useCallback(async (item: ItemToDelete) => {
     await deleteInvoiceMutation({
       variables: {
-        id
+        id: item.id,
       },
     });
     await invoiceQuery.client.resetStore();
     await invoiceQuery.refetch();
   }, [invoiceQuery, deleteInvoiceMutation]);
+
+  const handleCloseDeleteModal = React.useCallback((item?: ItemToDelete | null) => () => {
+    if (item) {
+      handleDelete(item);
+    }
+    setItemToDelete(null);
+  }, [handleDelete]);
+
+  const handleOpenDeleteModal = React.useCallback((item: ItemToDelete) => () => {
+    setItemToDelete(item);
+  }, []);
 
   const warrantiesCount = React.useMemo(() => {
     if (invoiceQuery.loading) {
@@ -70,14 +85,14 @@ const List = () => {
           <BsPencilSquare className="mr-sm-2"/>
         </OverlayTrigger>
         <OverlayTrigger overlay={<Tooltip id="tooltip-delete">Delete</Tooltip>}>
-          <BsTrash onClick={handleDelete(item.id)}/>
+          <BsTrash onClick={handleOpenDeleteModal(item)}/>
         </OverlayTrigger>
       </td>
       <td width="25%">
         <ProgressBar striped max={1} now={item.progress || 0} variant={(item.progress || 0) >= 1 ? 'danger' : 'primary'}/>
       </td>
     </tr>
-  )), [invoiceQuery.data?.invoiceAll.items, handleDelete]);
+  )), [invoiceQuery.data?.invoiceAll.items, handleOpenDeleteModal]);
 
   const loadMoreButton = React.useMemo(() => {
     const innerButton = loadingMore ? (
@@ -94,27 +109,43 @@ const List = () => {
   }, [invoiceQuery.data?.invoiceAll.hasMore, loadingMore, handleLoadMore]);
 
   return (
-    <Container fluid="sm" className="my-sm-4">
-      {warrantiesCount}
-      {!invoiceQuery.loading && (
-        <div className="d-flex flex-column align-items-center">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Item</th>
-                <th>Exp date</th>
-                <th>Actions</th>
-                <th>Progress to expire</th>
-              </tr>
-            </thead>
-            <tbody>
-              {items}
-            </tbody>
-          </Table>
-          {loadMoreButton}
-        </div>
-      )}
-    </Container>
+    <>
+      <Container fluid="sm" className="my-sm-4">
+        {warrantiesCount}
+        {!invoiceQuery.loading && (
+          <div className="d-flex flex-column align-items-center">
+            <Table striped bordered hover>
+              <thead>
+                <tr>
+                  <th>Item</th>
+                  <th>Exp date</th>
+                  <th>Actions</th>
+                  <th>Progress to expire</th>
+                </tr>
+              </thead>
+              <tbody>
+                {items}
+              </tbody>
+            </Table>
+            {loadMoreButton}
+          </div>
+        )}
+      </Container>
+      <Modal show={!!itemToDelete} onHide={handleCloseDeleteModal()}>
+        <Modal.Header closeButton>
+          <Modal.Title>Delete confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete "{itemToDelete?.item}"?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={handleCloseDeleteModal(itemToDelete)}>
+            Yes
+          </Button>
+          <Button variant="secondary" onClick={handleCloseDeleteModal()}>
+            No
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
